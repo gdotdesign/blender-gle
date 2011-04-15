4b1e25868e204c80be7e8367abce0d4687015d22
guszti5@hotmail.com <guszti5@hotmail.com>
2011-04-15 12:04:22 +0200

    Cleanup.

--- guard.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'rb-inotify'
-notifier = INotify::Notifier.new
-notifier.watch("Specs/Buttons", :modify) do |event|
-  puts event.inspect
-  puts "foo.txt was modified!"
-end
-notifier.run
